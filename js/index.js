f688b96ee9e74f8fa2c21e4dce50f28afe86e600
ThesplumCoder <michin505@hotmail.com>
2023-12-07 04:48:42 -0500

    Implementados los listados de las habitaciones y de los huespedes, junto con los formularios.

--- js/index.js
+++ js/index.js
@@ -1,60 +1,141 @@
 let app = angular.module("employee-view", []);
+const guestUrl = "http://localhost:8080/guest";
+const roomUrl = "http://localhost:8080/room";
+const rfidCardUrl = "http://localhost:8080/rfid-card";
+const espUrl = "http://espIp:8080/scan-card";
 
-app.controller("clients-controller", function($scope, $http) {
-let clientsCtrl = this;
+app.controller("guest-controller", function ($scope, $http) {
+  let guestC = this;
 
-clientsCtrl.clients = [];
+  guestC.guests = [];
 
-clientsCtrl.getAllClients = function() {
+  guestC.getAllGuests = function () {
     let config = {
-    headers: {
-        "proccess": "getAllClients"
-    }
-    }
-    $http.get("index.jsp", config)
-    .then( function(res) {
-        clientsCtrl.clients = res.data.Clients;
-    });
-}
+      headers: {
+        "Access-Control-Allow-Origin": guestUrl,
+      },
+    };
+    $http.get(guestUrl, config)
+      .then(function (res) {
+        guestC.guests = res.data;
+        console.log(guestC.guests);
+      });
+  };
 
-clientsCtrl.addOneClient = function() {
-    let config = {
-    headers: {
-        "proccess": "addOneClient"
-    }
-    }
+  guestC.addGuest = function () {
     let data = {
-    id: clientsCtrl.idClient,
-    name: clientsCtrl.nameClient,
-    telephoneNumber: clientsCtrl.telephoneNumberClient
-    }
-    console.log(data)
-
-    $http.post("index.jsp", data, config)
-    .then( function(res) {
+      id: guestC.id,
+      name: guestC.name,
+      telephoneNumber: guestC.telephoneNumber,
+    };
+    console.log(data);
+    $http.post(guestUrl, data)
+      .then(function (res) {
         console.log("Creation successful.");
         window.location.reload();
-    });
-}
+      });
+  };
 
-clientsCtrl.deleteById = function() {
-    let config = {
-    headers: {
-        "proccess": "deleteById"
-    }
-    }
+  guestC.deleteById = function () {
     let data = {
-    id: clientsCtrl.idClient,
-    }
-    console.log(data)
-
-    $http.post("index.jsp", data, config)
-    .then( function(res) {
+      id: guestC.id,
+    };
+    console.log(data);
+    $http.post(guestUrl, data)
+      .then(function (res) {
         console.log("Deletion successful.");
         window.location.reload();
-    });
-}
-window.onload = function() {
-    clientsCtrl.getAllClients();
-}
+      });
+  };
+
+  window.addEventListener("load", function () {
+    guestC.getAllGuests();
+  });
+});
+
+app.controller("room-controller", function ($scope, $http) {
+  let roomC = this;
+
+  roomC.rooms = [];
+
+  roomC.getAllRooms = function () {
+    let config = {
+      headers: {
+        "Access-Control-Allow-Origin": roomUrl,
+      },
+    };
+    $http.get(roomUrl, config)
+      .then(function (res) {
+        roomC.rooms = res.data;
+        console.log(roomC.rooms);
+      });
+  };
+
+  window.addEventListener("load", function () {
+    roomC.getAllRooms();
+  });
+});
+
+app.controller("rfid-card-controller", function ($scope, $http) {
+  let rfidC = this;
+
+  rfidC.rfidCards = [];
+
+  rfidC.getAllCards = function () {
+    let config = {
+      headers: {
+        "Access-Control-Allow-Origin": rfidCardUrl,
+      },
+    };
+    $http.get(rfidCardUrl, config)
+      .then(function (res) {
+        rfidC.rfidCards = res.data;
+        console.log(rfidC.rfidCards);
+      });
+  };
+
+  rfidC.addRfidCard = function () {
+    let data = {
+      room: {
+        id: rfidC.room,
+      },
+      status: {
+        id: 1,
+      },
+      uid: rfidC.uid,
+    };
+    let config = {
+      headers: {
+        "Access-Control-Allow-Origin": espUrl,
+      },
+    };
+    console.log(data);
+    $http.post(rfidCardUrl, data, config)
+      .then(function (res) {
+        console.log("Creation successful.");
+        window.location.reload();
+      });
+  };
+
+  rfidC.scanCard = function () {
+    rfidC.uid = null;
+
+    let config = {
+      headers: {
+        "Access-Control-Allow-Origin": espUrl,
+      },
+    };
+    $http.get(espUrl, config)
+      .then(function (res) {
+        rfidC.uid = res.data.uid;
+        console.log(rfidC.uid);
+      })
+      .catch(function (err) {
+        alert("The ESP doesn't respond.");
+      });
+  };
+
+  window.addEventListener("load", function () {
+    rfidC.getAllCards();
+  });
 });
