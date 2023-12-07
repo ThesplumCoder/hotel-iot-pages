2f52275f8f9bda31234fd3f0058eeb21b1c6e6ed
ThesplumCoder <andersonYacVpers002360@outlook.com>
2023-12-06 19:53:14 -0500

    Implementando las vistas

---
+++ js/guest.js
@@ -0,0 +1,1 @@
+let guest = 1
--- js/index.js
+++ js/index.js
@@ -1,1 +1,60 @@
-let a = 5
+let app = angular.module("employee-view", []);
+
+app.controller("clients-controller", function($scope, $http) {
+let clientsCtrl = this;
+
+clientsCtrl.clients = [];
+
+clientsCtrl.getAllClients = function() {
+    let config = {
+    headers: {
+        "proccess": "getAllClients"
+    }
+    }
+    $http.get("index.jsp", config)
+    .then( function(res) {
+        clientsCtrl.clients = res.data.Clients;
+    });
+}
+
+clientsCtrl.addOneClient = function() {
+    let config = {
+    headers: {
+        "proccess": "addOneClient"
+    }
+    }
+    let data = {
+    id: clientsCtrl.idClient,
+    name: clientsCtrl.nameClient,
+    telephoneNumber: clientsCtrl.telephoneNumberClient
+    }
+    console.log(data)
+
+    $http.post("index.jsp", data, config)
+    .then( function(res) {
+        console.log("Creation successful.");
+        window.location.reload();
+    });
+}
+
+clientsCtrl.deleteById = function() {
+    let config = {
+    headers: {
+        "proccess": "deleteById"
+    }
+    }
+    let data = {
+    id: clientsCtrl.idClient,
+    }
+    console.log(data)
+
+    $http.post("index.jsp", data, config)
+    .then( function(res) {
+        console.log("Deletion successful.");
+        window.location.reload();
+    });
+}
+window.onload = function() {
+    clientsCtrl.getAllClients();
+}
+});
